11549a38040299d4e388bb32ad6c7b26b93e6bf7
Chantal <Chantal13@users.noreply.github.com>
2025-08-01 23:38:44 -0400

    chore: remove dotenv and env module

--- quartz/util/env.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv"
-
-// Load environment variables from .env.local if present
-config({ path: ".env.local" })
-
-/** API token loaded from `.env.local` */
-export const APIToken = process.env.TOKEN || ""
